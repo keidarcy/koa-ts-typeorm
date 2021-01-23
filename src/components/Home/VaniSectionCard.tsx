c66dc1a2430220e5a7e00f33c50ca1ee0288dc11
XING YAHAO <keidarcy.1015@gmail.com>
2021-01-23 18:22:46 +0900

    update

--- src/components/Home/VaniSectionCard.tsx
+++ src/components/Home/VaniSectionCard.tsx
@@ -7,7 +7,7 @@ import {
   Stack,
   TextField
 } from '@shopify/polaris';
-import { useCallback, useContext, useMemo } from 'react';
+import { useCallback, useContext } from 'react';
 import { VaniContext } from '../../utils/contexts/VCScontext';
 import { useT } from '../../utils/hooks/useT';
 import { VaniActionEnum } from '../../utils/type.helper';
@@ -50,57 +50,54 @@ export const VaniSectionCard: React.FC<VaniSectionCardProps> = ({
     []
   );
 
-  return useMemo(
-    () => (
-      <>
-        <Layout.Section>
-          <Card>
-            <Card.Section>
-              <SettingToggle
-                action={{
-                  content: contentStatus,
-                  onAction: () => {
-                    dispatch({
-                      type: VaniActionEnum.CHANGE_CUSTOMIZE_VALUE,
-                      field: toogleField,
-                      value: !active
-                    });
-                  }
-                }}
-                enabled={active}
-              >
-                <Heading>{name}</Heading>
-                <p>{description}</p>
-              </SettingToggle>
-            </Card.Section>
-            <Card.Section subdued={!active}>
-              <Stack alignment="center" distribution="fill">
-                <Heading>{t.main.title}</Heading>
-                <TextField
-                  label={name}
-                  labelHidden
-                  disabled={!active}
-                  value={title}
-                  onChange={handleChange}
-                  placeholder={defaultTitle}
-                  align="left"
-                />
-              </Stack>
-            </Card.Section>
-            <Card.Section subdued={!active}>
-              <Stack alignment="center" distribution="equalSpacing">
-                <p>{t.main.showPages}</p>
-                {previewUrl && (
-                  <Link url={previewUrl} external>
-                    {t.main.preview}
-                  </Link>
-                )}
-              </Stack>
-            </Card.Section>
-          </Card>
-        </Layout.Section>
-      </>
-    ),
-    [dispatch]
+  return (
+    <>
+      <Layout.Section>
+        <Card>
+          <Card.Section>
+            <SettingToggle
+              action={{
+                content: contentStatus,
+                onAction: () => {
+                  dispatch({
+                    type: VaniActionEnum.CHANGE_CUSTOMIZE_VALUE,
+                    field: toogleField,
+                    value: !active
+                  });
+                }
+              }}
+              enabled={active}
+            >
+              <Heading>{name}</Heading>
+              <p>{description}</p>
+            </SettingToggle>
+          </Card.Section>
+          <Card.Section subdued={!active}>
+            <Stack alignment="center" distribution="fill">
+              <Heading>{t.main.title}</Heading>
+              <TextField
+                label={name}
+                labelHidden
+                disabled={!active}
+                value={title}
+                onChange={handleChange}
+                placeholder={defaultTitle}
+                align="left"
+              />
+            </Stack>
+          </Card.Section>
+          <Card.Section subdued={!active}>
+            <Stack alignment="center" distribution="equalSpacing">
+              <p>{t.main.showPages}</p>
+              {previewUrl && (
+                <Link url={previewUrl} external>
+                  {t.main.preview}
+                </Link>
+              )}
+            </Stack>
+          </Card.Section>
+        </Card>
+      </Layout.Section>
+    </>
   );
 };
